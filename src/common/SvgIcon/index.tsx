f97fa8d145dfbb5082b54d4dcde84197753b4844
Priyank raj <pry199826@gmail.com>
2022-01-27 23:17:20 +0530

    ImagePath

--- src/common/SvgIcon/index.tsx
+++ src/common/SvgIcon/index.tsx
@@ -1,5 +1,5 @@
 import { SvgIconProps } from "../types";
 
 export const SvgIcon = ({ src, width, height }: SvgIconProps) => (
-  <img src={`yahweeh_trust/public/img/svg/${src}`} alt={src} width={width} height={height} />
+  <img src={`/yahweeh_trust/public/img/svg/${src}`} alt={src} width={width} height={height} />
 );
