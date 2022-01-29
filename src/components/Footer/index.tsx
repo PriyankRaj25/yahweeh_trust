e7959da9feb857b5e5487fe05354430e6aab286f
Priyank raj <pry199826@gmail.com>
2022-01-29 13:34:38 +0530

    Mobile view correction

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -50,20 +50,19 @@ const Footer = ({ t }: any) => {
       <FooterSection>
         <Container>
           <Row justify="space-between">
-            <Col lg={10} md={10} sm={12} xs={12}>
+            <Col lg={10} md={10} sm={12} xs={14}>
               <Language>{t("Contact")}</Language>
               <Large to="/">{t("Tell us everything")}</Large>
               <Para>
                 {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
-              <a href="mailto:yahwehcharitabletrust@gmail.com">
-                <Chat>{t(`yahwehcharitabletrust@gmail.com`)}</Chat>
+                <Para>{t(`yahwehcharitabletrust@gmail.com`)}</Para>
                 <Para>Phone Number: +91-9035522703</Para>
-              </a>
+              
             </Col>
             <Col lg={10} md={10} sm={12} xs={12}>
               <Language>{t("Address")}</Language>
-              <Para>Yahweeh Charitable Trust for FCS</Para>
+              <Para>Yahweh Charitable Trust for FCS</Para>
               <Para>Regd. 00380-2021-22</Para>
               <Para>No 222, Standlishee Road</Para>
               <Para>Near Malik Rice Bazaar</Para>
